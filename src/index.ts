f0f01c1f854365c084b1b5a6a377de2f92883c53
caleb1248 <28stu647@lexingtonma.org>
2025-01-20 15:47:19 -0500

    fix annoying ts stuff

--- src/index.ts
+++ src/index.ts
@@ -15,32 +15,19 @@ interface Transports {
  * Anything with this type can be serialized using `JSON.stringify`.
  * To make this type work properly, add `strictNullChecks` to your `tsconfig.json`.
  */
-type ValidJson =
-  | string
-  | number
-  | boolean
-  | null
-  | ValidJson[]
-  | { [key: string]: ValidJson };
+type ValidJson = string | number | boolean | null | ValidJson[] | ValidJsonObject;
+
+interface ValidJsonObject extends Record<string, ValidJson> {}
 
 interface Connection extends Disposable {
-  sendRequest<T extends ValidJson>(
-    method: string,
-    params: ValidJson
-  ): Promise<T>;
+  sendRequest<T extends ValidJson>(method: string, params: ValidJson): Promise<T>;
   /**
    * Handles a request. If the handler returns a promise, the response will be sent when the promise is resolved.
    */
-  onRequest<T>(
-    method: string,
-    handler: (params: T) => ValidJson | Promise<ValidJson>
-  ): Disposable;
+  onRequest<T>(method: string, handler: (params: T) => ValidJson | Promise<ValidJson>): Disposable;
 
   sendNotification(method: string, params?: ValidJson): void;
-  onNotification(
-    method: string,
-    handler: (params: ValidJson) => void
-  ): Disposable;
+  onNotification(method: string, handler: (params: ValidJson) => void): Disposable;
 }
 
 interface RequestMessage extends Message {
@@ -60,10 +47,7 @@ interface ErrorMessage extends Message {
 }
 
 function createRequestQueue(transports: Transports) {
-  const queue = new Map<
-    number,
-    [(result: ValidJson) => void, (error: ValidJson) => void]
-  >();
+  const queue = new Map<number, [(result: ValidJson) => void, (error: ValidJson) => void]>();
   let currentId = 0;
   let disposed = false;
 
@@ -88,10 +72,7 @@ function createRequestQueue(transports: Transports) {
   });
 
   return {
-    sendRequest<T extends ValidJson>(
-      method: string,
-      params: ValidJson
-    ): Promise<T> {
+    sendRequest<T extends ValidJson>(method: string, params: ValidJson): Promise<T> {
       if (disposed) {
         throw new Error('Connection is disposed');
       }
@@ -152,10 +133,7 @@ function createConnection(transports: Transports): Connection {
   );
 
   return {
-    sendRequest<T extends ValidJson>(
-      method: string,
-      params: ValidJson
-    ): Promise<T> {
+    sendRequest<T extends ValidJson>(method: string, params: ValidJson): Promise<T> {
       return requestQueue.sendRequest<T>(method, params);
     },
 
@@ -205,12 +183,7 @@ abstract class BaseTransports implements Transports {
    * Fires a message to all registered handlers. Messages are validated, but non-messages will result in console warns, so *please* validate the messages yourself.
    */
   protected fireMessage(message: Message) {
-    if (
-      typeof message !== 'object' ||
-      message === null ||
-      message.portablerpc !== 'v1'
-    )
-      return;
+    if (typeof message !== 'object' || message === null || message.portablerpc !== 'v1') return;
     for (let i = 0; i < this._handlers.length; i++) {
       this._handlers[i](message);
     }
@@ -234,6 +207,7 @@ export { createConnection, BaseTransports };
 export type {
   Disposable,
   ValidJson,
+  ValidJsonObject,
   Message,
   RequestMessage,
   ResponseMessage,
